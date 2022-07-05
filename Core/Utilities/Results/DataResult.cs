4998937608339282f178e7bedf30b616d60bcc6b
Hasan Can Ozbek <hasancanozbekk@gmail.com>
2022-07-05 15:00:21 +0300

    Result structure added Core layer created

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Core.Utilities.Results
+{
+    public abstract class DataResult<T> : Result
+    {
+        public DataResult(T data, bool status, string message) : base(status, message)
+        {
+            Data = data;
+        }
+        public DataResult(T data, bool status) : base(status)
+        {
+            Data=data;
+        }
+        public T Data { get; set; }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Core.Utilities.Results
+{
+    public abstract class Result
+    {
+        public Result(bool status, string message) : this(status)
+        {
+            Message = message;
+        }
+        public Result(bool status)
+        {
+            Status=status;
+        }
+
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
